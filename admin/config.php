2510e4218629c163fc5448663f372f9a864ce622
Muhammet Yusuf Kızıl <56205966+muhammetyusufkizil@users.noreply.github.com>
2025-03-23 14:07:41 +0300

    Create config.php

---
+++ admin/config.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+$host = "localhost";
+$dbname = "personal_website";
+$username = "root";
+$password = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Bağlantı hatası: " . $e->getMessage());
+}
+?>
